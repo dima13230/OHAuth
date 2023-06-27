26a76715e31afdf4e94155da6aa920faa02e8c24
Dmitry Senyushkin <dsenyushkin.dev@gmail.com>
2023-06-28 02:27:40 +0300

    Add registration of board, challenge encryption

--- ohauthlib/src/lib.rs
+++ ohauthlib/src/lib.rs
@@ -1,26 +1,16 @@
 use rand::Rng;
-use std::char::MAX;
-use std::collections::btree_map::Range;
-use std::env;
-use std::fmt::format;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
 use std::path::Path;
 use std::time::Duration;
 
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
 
 use serialport as sp;
 use sp::*;
-use std::ffi::OsStr;
-use std::io::prelude::*;
-use udev::{Device, Enumerator};
+use udev::{Enumerator};
 
-use sha2::Sha256;
-
-use aes::cipher::{generic_array::GenericArray, BlockCipher, BlockDecrypt, BlockEncrypt, KeyInit};
+use aes::cipher::{generic_array::GenericArray, BlockDecrypt, BlockEncrypt, KeyInit};
 use aes::Aes256;
 
 #[derive(Serialize, Deserialize)]
@@ -48,8 +38,92 @@ const XOR_KEY: &str = "OHAuthDefaultXOR";
 const JSON_KEY: &str = "OHAuthDefaultJSONKey";
 
 pub fn register() -> OHAuthResult {
-    // TODO
-    OHAuthResult::Success
+    let auth_boards_json_path = Path::new(BOARDS_JSON_FILENAME);
+    let mut auth_boards_json_file: File;
+    let mut auth_boards: AuthorizedBoards = AuthorizedBoards { boards: vec![] };
+
+    let key_bytes_json = JSON_KEY.as_bytes();
+    let key_json = GenericArray::clone_from_slice(key_bytes_json);
+
+    let cipher_json = Aes256::new(&key_json);
+    if auth_boards_json_path.exists() {
+        auth_boards_json_file = match File::open(auth_boards_json_path) {
+            Err(why) => {
+                return OHAuthResult::Error(
+                    format!("Registered boards file exists but opening it wasn't successful. {}", why),
+                )
+            }
+            Ok(file) => file,
+        };
+
+        let mut boards_raw: Vec<u8> = vec![];
+        match auth_boards_json_file.read_to_end(&mut boards_raw) {
+            Err(why) => {
+                return OHAuthResult::Error(format!("Couldn't read registered boards file. {}", why))
+            }
+            Ok(_) => ()
+        }
+
+        let mut boards_raw_generic = GenericArray::clone_from_slice(boards_raw.as_slice());
+        cipher_json.decrypt_block(&mut boards_raw_generic);
+
+        auth_boards = serde_json::from_slice(&boards_raw_generic.as_slice()).unwrap();
+    } else {
+        auth_boards_json_file = match File::create(auth_boards_json_path) {
+            Err(why) => {
+                return OHAuthResult::Error(format!("Couldn't create registered boards file. {}", why))
+            },
+            Ok(file) => file
+        };
+    }
+
+    // prepare AES256 cipher
+    let key_bytes = AES_KEY.as_bytes();
+    let key = GenericArray::clone_from_slice(key_bytes);
+    let cipher = Aes256::new(&key);
+
+    let mut enumerator = Enumerator::new().expect("Failed to create udev enumerator");
+    enumerator
+        .match_subsystem("tty")
+        .expect("Failed to set subsystem filter");
+
+    enumerator
+        .match_property("ID_MODEL", USB_PRODUCT_NAME)
+        .expect("Failed to set ID_MODEL filter");
+
+    for device in enumerator.scan_devices().unwrap() {
+        println!("{}", device.devnode().unwrap().to_str().unwrap());
+        let mut port = serialport::new(device.devnode().unwrap().to_str().unwrap(), 115200)
+            .timeout(Duration::from_secs(15))
+            .stop_bits(StopBits::One)
+            .data_bits(DataBits::Eight)
+            .parity(Parity::None)
+            .open_native()
+            .expect(format!("Failed to open port {}", device.devnode().unwrap().to_str().unwrap()).as_str());
+
+        // Arduino boards reset after Serial connection is established, so we have to have delay before we continue working with it
+        std::thread::sleep(std::time::Duration::from_secs(2));
+
+        let board_hash = match exchange_challenge(&mut port, &cipher) {
+            Ok(string) => string,
+            Err(why) => return OHAuthResult::Error(format!("Challenge test not passed. {}", why))
+        };
+        
+        auth_boards.boards.push(board_hash);
+        let auth_boards_json = match serde_json::to_string(&auth_boards) {
+            Ok(string) => string,
+            Err(e) => return OHAuthResult::Error(format!("Couldn't convert auth_boards to JSON. {}", e))
+        };
+
+        let mut auth_boards_json_bytes_encrypted = GenericArray::clone_from_slice(auth_boards_json.as_bytes());
+        cipher_json.encrypt_block(&mut auth_boards_json_bytes_encrypted);
+
+        match auth_boards_json_file.write_all(&auth_boards_json_bytes_encrypted.as_slice()) {
+            Ok(_) => return OHAuthResult::Success,
+            Err(why) => return OHAuthResult::Error(format!("Couldn't write to registered boards file! {}", why))
+        };
+    }
+    OHAuthResult::Error("No functional OHAuth keys are connected to this device.".to_string())
 }
 
 pub fn authorize() -> OHAuthResult {
@@ -71,7 +145,7 @@ pub fn authorize() -> OHAuthResult {
             Err(why) => {
                 return OHAuthResult::Error(format!("Couldn't read registered boards file. {}", why))
             }
-            Ok(_) => println!("{} exists and is readable.", BOARDS_JSON_FILENAME),
+            Ok(_) => ()
         }
         let key_bytes = JSON_KEY.as_bytes();
         let key = GenericArray::clone_from_slice(key_bytes);
@@ -110,21 +184,23 @@ pub fn authorize() -> OHAuthResult {
             .open_native()
             .expect(format!("Failed to open port {}", device.devnode().unwrap().to_str().unwrap()).as_str());
 
+        // Arduino boards reset after Serial connection is established, so we have to have delay before continue working with it
         std::thread::sleep(std::time::Duration::from_secs(2));
 
-        let board_hash: String;
-        match exchange_challenge(&mut port, &cipher) {
-            Ok(string) => board_hash = string,
-            Err(e) => return OHAuthResult::Error(format!("Challenge test not passed. {}", e))
-        }
+        let board_hash = match exchange_challenge(&mut port, &cipher) {
+            Ok(string) => string,
+            Err(why) => return OHAuthResult::Error(format!("Challenge test not passed. {}", why))
+        };
         
         for board in &auth_boards.boards {
             if board_hash.eq(board) {
                 return OHAuthResult::Success
             }
         }
+
+        return OHAuthResult::NotRegistered
     }
-    return OHAuthResult::Error("No functional OHAuth keys are connected to this device.".to_string());
+    return OHAuthResult::Error("No functional OHAuth keys are connected to this device.".to_string())
 }
 
 fn exchange_challenge(port: &mut TTYPort, cipher: &Aes256) -> serialport::Result<String> {
@@ -144,7 +220,7 @@ fn exchange_challenge(port: &mut TTYPort, cipher: &Aes256) -> serialport::Result
     match port.write(&challenge_enc) {
         Ok(_) => (),
         Err(e) => return Err(e.into())
-    }
+    };
     
     // Read response with hash and challenge value as AES256 encrypted JSON
     let mut serial_buf = [0u8; 1000];
@@ -153,7 +229,7 @@ fn exchange_challenge(port: &mut TTYPort, cipher: &Aes256) -> serialport::Result
     let mut response = GenericArray::clone_from_slice(&serial_buf);
     cipher.decrypt_block(&mut response);
 
-    let response_json: BoardResponse = serde_json::from_slice(&response).unwrap();
+    let response_json: BoardResponse = serde_json::from_slice(&response.as_slice()).unwrap();
 
     // Now transform challenge and compare
     transform_challenge(&mut challenge, XOR_KEY.as_bytes());
